ed4b2d1a63bed454481034da9c5ba6ea8211446d
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-05-07 20:21:40 +0530

    Initial Commit

---
+++ OnBoardingScreensWithSwiftUI/OnBoardingScreensWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingScreensWithSwiftUIApp.swift
+//  OnBoardingScreensWithSwiftUI
+//
+//  Created by MANAS VIJAYWARGIYA on 07/05/21.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingScreensWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
